17eb47d03572f21661ad0d4134e58851cc6ebf8a
Dinesh Babu Rengasamy <dineshbabur92@gmail.com>
2016-03-02 20:20:40 +0530

    start with auth controller

--- app/signup/signup_controller.js
+++ app/signup/signup_controller.js
@@ -1,9 +1,21 @@
 (function(){
     
     angular.module("socialNetwork")
-        .controller("signupController", ["$scope", function(){
+        .controller("signupController", ["$scope", "$state", "$http", function($scope, $state, $http){
             
-            
+            $scope.createUser = function(){
+                
+                $http.post("/signup", {"email": $scope.email, "password": $scope.password}, function(response){
+                    
+                    console.log(response);
+                    
+                }, function(error){
+                    
+                    console.log(error);
+                    
+                });
+                
+            };
             
             
         }]);
--- server.js
+++ server.js
@@ -3,8 +3,14 @@ var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 
 var app = express();
+var authController = require(controllers/authController);
 
 mongoose.connect("mongodb://localhost:27017/social_network");
+
+
+app.use("/app", express.static(__dirname + "/app"));
+//app.use("/node_modules", express.static(__dirname + "/node_modules"));
+
 
 app.get("/",function(req, res){
     
@@ -12,8 +18,7 @@ app.get("/",function(req, res){
     
 });
 
-app.use("/app", express.static(__dirname + "/app"));
-app.use("/node_modules", express.static(__dirname + "/node_modules"));
+app.post("/signup", authenticationController.signup);
 
 app.listen(3000, function(){
     
